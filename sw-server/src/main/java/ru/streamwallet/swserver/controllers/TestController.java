e86342ca9da1b3efa866767e020bb2118cf7e332
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-03-28 16:46:38 +0200

    Added TestController and fix

---
+++ sw-server/src/main/java/ru/streamwallet/swserver/controllers/TestController.java
@@ -0,0 +1,19 @@
+package ru.streamwallet.swserver.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Artur Belogur on 28.03.17.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public ResponseEntity<?> testServer() {
+        return new ResponseEntity<String>("Hello", HttpStatus.OK);
+    }
+}
